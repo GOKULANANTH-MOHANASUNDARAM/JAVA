978df111ed2cbe7152dcd2e5087ff19e3bfff84d
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-02-27 18:59:42 +0530

    changed method name bro

--- Day4/ThrowIOException.java
+++ Day4/ThrowIOException.java
@@ -6,16 +6,12 @@ import java.io.IOException;
 public class ThrowIOException {
 	public static void main(String[] args) throws IOException{
 		ThrowException te = new ThrowException();
-		//te.throwIO();
-		te.sample();
+		te.searchFile();
 	}
 }
 
 class ThrowException{
-//	public void throwIO() throws IOException {
-//		throw new IOException("IOException occured");
-//	}
-	public void sample() throws IOException {
+	public void searchFile() throws IOException {
 		FileReader newfile = new FileReader("Exceptions.txt");
 		System.out.println(newfile.read());
 		newfile.close();
