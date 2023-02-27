45d5ebec9f82c068a7e99a61ffa8f6db6aa4afb1
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-02-27 18:49:20 +0530

    Done changes

--- Day4/FormatException.java
+++ Day4/FormatException.java
@@ -6,34 +6,34 @@ public class FormatException {
 
 	public static void main(String args[]) throws Exception{
 
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		int average = 0;
 		while(average == 0) {
 			try {
 			
 				System.out.println("Enter mark of subject 1 (Tamil) : ");
-				String sub1 = sc.nextLine();
-				int subject_1 = Integer.parseInt(sub1);
+				String sub1 = scanner.nextLine();
+				int subject1 = Integer.parseInt(sub1);
 		
 				System.out.println("Enter mark of subject 2 (English): ");
-				String sub2 = sc.nextLine();
-				int subject_2 = Integer.parseInt(sub2);
+				String sub2 = scanner.nextLine();
+				int subject2 = Integer.parseInt(sub2);
 				
 				System.out.println("Enter mark of subject 3 (Maths) : ");
-				String sub3=sc.nextLine();
-				int subject_3 = Integer.parseInt(sub3);
+				String sub3=scanner.nextLine();
+				int subject3 = Integer.parseInt(sub3);
 
 				System.out.println("Enter mark of subject 4 (Science) : ");
-				String sub4=sc.nextLine();
-				int subject_4 = Integer.parseInt(sub4);
+				String sub4=scanner.nextLine();
+				int subject4 = Integer.parseInt(sub4);
 
 				System.out.println("Enter mark of subject 5 (Social Science)");
-				String sub5=sc.nextLine();
-				int subject_5 = Integer.parseInt(sub5);
+				String sub5=scanner.nextLine();
+				int subject5 = Integer.parseInt(sub5);
 
 		
-				average=(subject_1+subject_2+subject_3+subject_4+subject_5)/5;
-				System.out.println("Average Mark is "+average);
+				average = (subject1 + subject2 + subject3 + subject4 + subject5) / 5;
+				System.out.println("Average Mark is " + average);
 			
 			}
 			catch(NumberFormatException e) {
