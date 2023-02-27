0d0a03f583b8be10f6094bc31c4f49b2704ca65e
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-02-27 18:56:48 +0530

    changed the name of the scanner

--- Day4/Java9.java
+++ Day4/Java9.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Java9 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		int i=1;
 		while(i==1) {
 			System.out.println("enter the number: ");	
-			int n = sc.nextInt();
+			int n = scanner.nextInt();
 			try {
 				if(n<0 || n>100) {
 					throw new OutOfRangeException();
