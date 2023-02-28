d7c9851e3d2914f9bbcb980a61b48a953bf90b85
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-02-28 19:34:30 +0530

    changed thread process

--- day5/CallThreadTwoTimes.java
+++ day5/CallThreadTwoTimes.java
@@ -1,24 +1,14 @@
 package day5;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class CallThreadTwoTimes {
-	public static void main(String[] args) {
-		ExecutorService es=Executors.newFixedThreadPool(1);
-		es.execute(()->{
-			Numbers.printNumber();
-		});
-		es.shutdown();
-		
-//		es.execute(()->{
-//			Numbers.printNumber();
-//		});
-//		es.shutdown();
+	public static void main(String[] args) throws Exception{
+		Numbers number = new Numbers();
+		number.start();
+		number.start();
 	}
 }
 
-class Numbers{
+class Numbers extends Thread{
 	public static void printNumber() {
 		for(int i=0;i<5;i++) {
 			System.out.println(i);
