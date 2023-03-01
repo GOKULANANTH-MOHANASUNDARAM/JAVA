10f7d937fd16dacc893c6dd868feb2220cec6455
GOKULANANTH-MOHANASUNDARAM <gokulananth.mohanasundaram@cdw.com>
2023-03-01 13:58:23 +0530

    Update CommandPattern.java

--- Patterns (Exercise-4)/CommandPattern.java
+++ Patterns (Exercise-4)/CommandPattern.java
@@ -1,134 +1,77 @@
 package exercise4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommandPattern {
 	public static void main(String[] args) {
-		UniversalRemote genie = new UniversalRemote();
 		
-		Tv tv =  new Tv();
-		SetTopBox sbox = new SetTopBox();
-		SoundSystem ss = new SoundSystem();
-		VGame vgame = new VGame();
+		Stock stock = new Stock();
 		
-		Command newscommand = new NewsChannelCommand(tv, sbox, ss, vgame);
-		Command serialcommand = new SerialChannelCommand(tv, sbox, ss, vgame);
-		Command ttgamecommand = new TTGameCommand(tv, sbox, ss, vgame);
-		
-		genie.assignCommand (1, ttgamecommand);
-		genie.assignCommand (2, newscommand);
-		genie.assignCommand (3, serialcommand);
-		
-		genie.executeCommand (1);
+	    BuyStock buyStockOrder = new BuyStock(stock);
+	    SellStock sellStockOrder = new SellStock(stock);
+
+	    Broker broker = new Broker();
+	    broker.takeOrder(buyStockOrder);
+	    broker.takeOrder(sellStockOrder);
+
+	    broker.placeOrders();
 	}
 }
 
-abstract class Command {
-	Tv tv; SetTopBox sbox; SoundSystem ss; VGame vgame;
-	
-	public Command() {
-		
-	}
-	
-	public Command(Tv tv, SetTopBox sbox, SoundSystem ss, VGame vgame) {
-		this.tv = tv;
-		this.sbox = sbox;
-		this.ss = ss;
-		this.vgame = vgame;
-	}
-	
-	public abstract void execute();
+interface Order {
+	   void execute();
 }
 
-class UniversalRemote{
-	Command c[] = new Command[5];
-	public UniversalRemote() {
-		for(int i=0; i<5; i++) {
-			c[i]=new DummyCommand();
-		}
-	}
+class Stock {
 	
-	public void executeCommand(int slot) {
-		c[slot].execute();
+	private String name = "Nifty";
+	private int quantity = 10;
+
+	public void buy(){
+		System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] bought");
 	}
-	
-	public void assignCommand(int slot,Command command) {
-		c[slot]=command;
+	public void sell(){
+	    System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] sold");
 	}
 }
 
-class DummyCommand extends Command {
-	@Override
+class BuyStock implements Order {
+	private Stock stock;
+
+	public BuyStock(Stock stock){
+	    this.stock = stock;
+	}
+
 	public void execute() {
-		System.out.println("i am a dummy slot.....yet to be assigned a process...");
+	    stock.buy();
 	}
 }
-class NewsChannelCommand extends Command {
-	public NewsChannelCommand (Tv tv, SetTopBox sbox, SoundSystem ss, VGame vgame) {
-		super (tv,sbox,ss,vgame);
+
+class SellStock implements Order {
+	private Stock stock;
+
+	public SellStock(Stock stock){
+	    this.stock = stock;
 	}
-	@Override
+
 	public void execute() {
-		System.out.println("the news channel process started...");
-		tv.av1();
-		sbox.newChannel();
-		ss.increaseVolume();		
-		System.out.println("enjoy the news channel....");
+	    stock.sell();
 	}
 }
-class SerialChannelCommand extends Command {
-	public SerialChannelCommand(Tv tv, SetTopBox sbox, SoundSystem ss, VGame vgame) {
-		super(tv, sbox, ss, vgame);
+
+class Broker {
+	private List<Order> orderList = new ArrayList<Order>(); 
+
+	public void takeOrder(Order order){
+	   orderList.add(order);		
 	}
-	@Override
-	public void execute() {
-		System.out.println("the serial channel process started...");
-		tv.av1();
-		sbox.serialChannel();
-		ss.increaseVolume();		
-		System.out.println("enjoy the saas bahu serial channel....");
-	}
-}
-class TTGameCommand extends Command {
-	public TTGameCommand(Tv tv, SetTopBox sbox, SoundSystem ss, VGame vgame) {
-		super(tv, sbox, ss, vgame);
-	}
-	@Override
-	public void execute() {
-		System.out.println("the ttgame process started...");
-		tv.av2();
-		vgame.ttgame();
-		ss.decreaseVolume();		
-		System.out.println("enjoy the tt game....");
-	}
-}
-class Tv {
-	public void av1() {
-		System.out.println("av1 mode...");
-	}
-	public void av2() {
-		System.out.println("av2 mode...");
-	}
-}
-class SetTopBox {
-	public void newChannel() {
-		System.out.println("news channel...");
-	}
-	public void serialChannel() {
-		System.out.println("mamiyar marumaga serial channel...");
-	}
-}
-class SoundSystem {
-	public void increaseVolume() {
-		System.out.println("volume increased...");
-	}
-	public void decreaseVolume() {
-		System.out.println("volume decreased...");
-	}
-}
-class VGame {
-	public void ttgame() {
-		System.out.println("tt game started...");
-	}
-	public void snooker() {
-		System.out.println("snooker game...");
+
+	public void placeOrders(){
+	   
+	   for (Order order : orderList) {
+		   order.execute();
+	   }
+	   orderList.clear();
 	}
 }
