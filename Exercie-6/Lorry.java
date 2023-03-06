69cba6686dd083c3a944b4d63f9befb8218ee2a5
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-03-07 00:28:38 +0530

    updated the lorry problem

--- Exercie-6/Lorry.java
+++ Exercie-6/Lorry.java
@@ -1,58 +1,90 @@
 package exercise6;
 
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.DayOfWeek;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Lorry {
-	public static void main(String[] args) {
-		double speed = 60;
-
-        // Distance to travel in km
-        double distance = 1000;
-
-        // Date and time of departure
-        LocalDate date = LocalDate.of(2023, Month.MARCH, 1);
-        LocalTime time = LocalTime.of(8, 0);
-
-        // Calculate the estimated time of arrival
-        LocalDateTime currentDateTime = LocalDateTime.of(date, time);
-        Duration timeTaken = Duration.ofMinutes((long) ((distance / speed) * 60));
-        LocalDateTime estimatedDateTime = currentDateTime.plus(timeTaken);
-
-        // Check for holidays and working hours
-        while (!isWorkingDay(estimatedDateTime.toLocalDate())) {
-            estimatedDateTime = estimatedDateTime.plusDays(1);
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the starting date in yyyy-mm-dd format: ");
+        String startdate = scanner.nextLine();
+        System.out.print("Enter the travel distance in km: ");
+        int distance = scanner.nextInt();
+        System.out.print("Enter the speed in km/h: ");
+        int speed = scanner.nextInt();
+        System.out.print("Enter the starting time in hh:mm format (24-hour clock): ");
+        String starttime = scanner.next();
+        
+        LocalDate startDate = LocalDate.parse(startdate);
+        LocalTime startTime = LocalTime.parse(starttime);
+        
+        int totalHours = distance / speed;
+        int totalDays = totalHours / 8;
+        
+        if (totalHours % 8 != 0) {
+            totalDays++;
         }
-        if (estimatedDateTime.getHour() >= 20) {
-            estimatedDateTime = estimatedDateTime.plusHours(12);
-        } else if (estimatedDateTime.getHour() < 8) {
-            estimatedDateTime = estimatedDateTime.withHour(8);
-        }
-
-        // Print the estimated date and time of arrival
-        System.out.println("Estimated date of arrival: " + estimatedDateTime.toLocalDate());
-        System.out.println("Estimated time of arrival: " + estimatedDateTime.toLocalTime());
-    }
-	
-	// Check if a given date is a working day or not
-    public static boolean isWorkingDay(LocalDate date) {
-        // Check for Sunday and second Saturday holiday
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        if (dayOfWeek == DayOfWeek.SUNDAY) {
-            return false;
-        } else if (dayOfWeek == DayOfWeek.SATURDAY) {
-            int dayOfMonth = date.getDayOfMonth();
-            if (dayOfMonth >= 8 && dayOfMonth <= 14) {
-                return false;
+        
+        List<LocalDate> holidays = Arrays.asList(
+            LocalDate.of(startDate.getYear(), Month.JANUARY, 1), 
+            LocalDate.of(startDate.getYear(), Month.AUGUST, 15), 
+            LocalDate.of(startDate.getYear(), Month.JANUARY, 26) 
+        );
+        
+        List<DayOfWeek> weekends = Arrays.asList(
+            DayOfWeek.SUNDAY, 
+            DayOfWeek.SATURDAY 
+        );
+        
+        LocalDateTime currentDateTime = LocalDateTime.of(startDate, startTime);
+        LocalDate currentDate = currentDateTime.toLocalDate();
+        LocalTime currentTime = currentDateTime.toLocalTime();
+        
+        for (int i = 0; i < totalDays; i++) {
+        	// Check if current date is a holiday or weekend
+            if (holidays.contains(currentDate) || weekends.contains(currentDate.getDayOfWeek())) {
+                // Skip this day
+                currentDate = currentDate.plusDays(1);
+                continue;
             }
+            
+            // Calculate the number of hours left in the current day
+            int remainingHours = 8 - currentTime.getHour();
+            
+            // If this is the last day, adjust the remaining hours
+            if (i == totalDays - 1 && totalHours % 8 != 0) {
+                remainingHours = totalHours % 8;
+            }
+            
+            // Calculate the time the lorry will reach the destination on this day
+            LocalTime destinationTime = currentTime.plusHours(remainingHours);
+            
+            // If the lorry will reach the destination after 8pm, adjust the time to 8pm
+            if (destinationTime.isAfter(LocalTime.of(20, 0))) {
+                destinationTime = LocalTime.of(20, 0);
+            }
+            
+            // Calculate the date the lorry will reach the destination on this day
+            LocalDate destinationDate = currentDate.plusDays(i);
+            
+            // Create a LocalDateTime object for the destination date and time
+            LocalDateTime destinationDateTime = LocalDateTime.of(destinationDate, destinationTime);
+            
+            // Print the destination date and time
+            System.out.println("The lorry will reach the destination on " + destinationDateTime);
+            
+            // Update the current date and time for the next day
+            currentDate = destinationDate.plusDays(1);
+            currentTime = LocalTime.of(8, 0);
         }
-
-        // Check for other holidays
-        MonthDay holiday1 = MonthDay.of(Month.JANUARY, 1);
-        MonthDay holiday2 = MonthDay.of(Month.AUGUST, 15);
-        MonthDay holiday3 = MonthDay.of(Month.JANUARY, 26);
-        if (date.getMonth().equals(holiday1) || date.getMonth().equals(holiday2) || date.getMonth().equals(holiday3)) {
-            return false;
-        }
-        return true;
+        scanner.close();
     }
+    
 }
