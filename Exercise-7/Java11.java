cbb3feafd6d908b37b1ea57368932d9a26e6eaa6
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-03-08 10:59:28 +0530

    exception catch is updated

--- Exercise-7/Java11.java
+++ Exercise-7/Java11.java
@@ -3,13 +3,16 @@ package exercise7;
 import java.io.File;
 
 public class Java11 {
-	public static void main(String[] args) {
-		File file = new File("/Users/gokulananth/eclipse-workspace/Java-Excercise/src/day1");
-		if (file.isDirectory()) {
-			String string[] = file.list();
-			for (int i = 0; i < string.length; i++) {
-				System.out.println(string[i]);
+	public static void main(String[] args) throws Exception {
+		try {
+			File file = new File("/Users/gokulananth/eclipse-workspace/Java-Excercise/src/day1");
+			if (file.isDirectory()) {
+				String string[] = file.list();
+				for (int i = 0; i < string.length; i++) {
+					System.out.println(string[i]);
+				}
 			}
-		}
+		} catch (exception e) {
+			System.out.println(e);
 	}
 }
