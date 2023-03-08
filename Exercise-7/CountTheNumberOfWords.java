92f5fe15b5f39904277039be3724b630b328ae42
GOKULANANTH-MOHANASUNDARAM <125631873+GOKULANANTH-MOHANASUNDARAM@users.noreply.github.com>
2023-03-08 11:38:26 +0530

    added comments to the code

--- Exercise-7/CountTheNumberOfWords.java
+++ Exercise-7/CountTheNumberOfWords.java
@@ -10,18 +10,18 @@ public class CountTheNumberOfWords {
 		Scanner scanner = new Scanner(System.in);
 		String string = scanner.nextLine();
 		int count = 0;
-		try(BufferedReader reader = new BufferedReader(new StringReader(string))){
+		try(BufferedReader reader = new BufferedReader(new StringReader(string))){   // Read the input String, using BufferReader
 			String str;
-			while((str = reader.readLine())!=null) {
-				String[] words = str.split("\\s+");
-				count += words.length;
+			while((str = reader.readLine())!=null) {	// Upto the string got null
+				String[] words = str.split("\\s+");	// Split using space
+				count += words.length;			// count the no. of words
 			}
 			
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("No.of words " + count);
+		System.out.println("No.of words " + count);		// prints the count
 		scanner.close();
 	}
 
